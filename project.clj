15787fd50a00be93d9331742ffafb5c27da5d3c5
Torsten Mangner <tmangner@inoxio.de>
2015-07-09 17:31:57 +0200

    version 0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/oscillator "0.0.1-SNAPSHOT"
+(defproject de.otto/oscillator "0.1.0"
             :description "A Clojure library that lets you create dashboards with
                          interactive charts to monitor applications in multiple environments."
             :url "https://github.com/otto-de/oscillator"
