39adfcd7147116a86fa8579f6b33ecf140d57693
Torsten Mangner <tmangner@inoxio.de>
2015-10-23 10:26:27 +0200

    Removed "Go back to defaults after 10mins"

--- src-cljs/monitor/back_to_default.cljs
+++
@@ -1,7 +0,0 @@
-(ns monitor.back-to-default)
-
-(defn go-back-to-default-page-after [timeout]
-  (.log js/console "going back to defaults after %d mins" (/ timeout 60 1000))
-  (js/setTimeout #(aset js/document "location" "href" "/") timeout))
-
-(go-back-to-default-page-after (* 10 60 1000))
